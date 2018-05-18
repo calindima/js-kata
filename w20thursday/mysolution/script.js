d88c1724f162d1619edde731a8c929095a6dec2a
calindima <calin.dima@yahoo.com>
2018-05-18 13:16:12 +0200

    thursday done

---
+++ w20thursday/mysolution/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// listen for clicks on the color-selectors
+const container = document.querySelector('#colorselector');
+container.addEventListener('click', e => {
+    //console.log(e);
+    const clickedLi = e.target;
+    // console.log(clickedLi.dataset.color);
+    document.documentElement.style.setProperty("--basecolor", clickedLi.dataset.color);
+});
+// when one is clicked:
+
+    // find the element that was clicked
+
+    // find the color from that element
+
+    // set the css variable --basecolor 
